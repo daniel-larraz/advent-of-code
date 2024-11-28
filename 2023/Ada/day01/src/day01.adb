30b18ba7c336cfd774b93229378e0f259061a825
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2024-11-28 11:54:51 -0600

    2023: Prove input file in Day01 is always closed

--- 2023/Ada/day01/src/day01.adb
+++ 2023/Ada/day01/src/day01.adb
@@ -5,27 +5,45 @@ with Day01_Pkg; use Day01_Pkg;
 
 procedure Day01 is
 
-   procedure Process_File (
-      Filename : String;
-      Only_Numerals : Boolean)
-   with
-      Exceptional_Cases =>
-         (Name_Error | Use_Error | End_Error => Standard.True);
+   procedure Open_Input_File (
+      File : in out File_Type;
+      Name : String
+   ) with
+      Pre  => not Is_Open (File),
+      Post => not Is_Open (File) or else Mode (File) = In_File;
 
-   procedure Process_File (
-      Filename : String;
+   procedure Open_Input_File (
+      File : in out File_Type;
+      Name : String
+   ) is
+   begin
+      Open (File, In_File, Name);
+   exception
+      when Name_Error =>
+         Put ("Error: File '"); Put (Name); Put_Line ("' does not exist.");
+      when Use_Error =>
+         Put ("Error: Couldn't open file '"); Put (Name); Put_Line ("'.");
+   end Open_Input_File;
+
+   procedure Process_File_and_Close (
+      File          : in out File_Type;
+      Only_Numerals : Boolean
+   ) with
+      Pre  => Is_Open (File) and then Mode (File) = In_File,
+      Post => not Is_Open (File);
+
+   procedure Process_File_and_Close (
+      File          : in out File_Type;
       Only_Numerals : Boolean
    ) is
-      File    : File_Type;
       Success : Boolean := False;
       Sum     : Natural := 0;
       Line    : String (1 .. 1024) with Relaxed_Initialization;
       Last    : Natural;
    begin
-      Open (File, In_File, Filename);
       while not End_Of_File (File) loop
          Get_Line (File, Line, Last);
-         if Line'First <= Last and then Last <= Line'Last then
+         if Line'First <= Last then
             declare
                Input : constant String := Line (Line'First .. Last);
             begin
@@ -36,18 +54,23 @@ procedure Day01 is
          end if;
          exit when not Success;
       end loop;
-      pragma Warnings (Off);
       Close (File);
-      pragma Warnings (On);
       Process_Result (Success, Sum);
    exception
-      when others =>
-         if Is_Open (File) then
-            pragma Warnings (Off);
-            Close (File);
-            pragma Warnings (On);
-         end if;
-         raise;
+      when End_Error => Close (File);
+   end Process_File_and_Close;
+
+   procedure Process_File (
+      Filename      : String;
+      Only_Numerals : Boolean
+   ) is
+      File : File_Type;
+   begin
+      Open_Input_File (File, Filename);
+      if Is_Open (File) then
+         Process_File_and_Close (File, Only_Numerals);
+      end if;
+      pragma Assert (not Is_Open (File));
    end Process_File;
 
    procedure Process_File_Part_1 (Filename : String) is
@@ -69,11 +92,4 @@ begin
    Process_File_Part_2 ("./share/day01/example_part_2.txt");
    Put_Line ("# Input - Part 2 #");
    Process_File_Part_2 ("./share/day01/input.txt");
-exception
-   when Name_Error =>
-      Put_Line ("Error: File does not exist.");
-   when Use_Error =>
-      Put_Line ("Error while processing input file.");
-   when End_Error =>
-      Put_Line ("Unexpected error: end of file reached.");
 end Day01;
